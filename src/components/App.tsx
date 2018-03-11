184a3083d4ca42c2932629e53bb4beb437d1b203
Swapnil Raj <raj.swapnil99@gmail.com>
2018-03-11 14:00:36 +0000

    Remove placeholder stuff, cleanup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,13 +6,7 @@ import * as React from 'react';
 
 import { classes, style } from 'typestyle';
 
-import { ITimerState } from '../stores/timer';
-
 import { normalizeEl } from '../utils/styles';
-
-import { renderIf } from '../utils/render-if-else';
-
-import Time from './Time';
 
 const mainContainer = style({
   alignItems: 'center',
@@ -21,26 +15,9 @@ const mainContainer = style({
   justifyContent: 'center',
 });
 
-interface IAppProps extends ITimerState {
-  startTimer();
-  stopTimer();
-}
-
-const App = ({
-  isRunning,
-  startTimer,
-  stopTimer,
-  time,
-}: IAppProps) => (
+const App = () => (
   <div className={classes(normalizeEl, mainContainer)}>
-    <main className={mainContainer}>
-      <Time time={time} />
-      {renderIf(
-        isRunning,
-        <button onClick={stopTimer}>Stop</button>,
-        <button onClick={startTimer}>Start</button>,
-      )}
-    </main>
+    <main className={mainContainer}>Hii</main>
   </div>
 );
 
