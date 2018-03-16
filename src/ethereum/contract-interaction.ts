55f3d1a814200d2314485f5819f3e21767207818
Ebin-Benny <bennye@tcd.ie>
2018-03-16 12:36:03 +0000

    Added functions to help with searching.

--- src/ethereum/contract-interaction.ts
+++ src/ethereum/contract-interaction.ts
@@ -11,43 +11,43 @@ const managerAddress = '0x410a4bcb61bfe8ffe2cb281d12cfbb61d345c6a8';
 managerContract.options.address = managerAddress;
 
 export interface IBetInfo {
-   /** First team, typically the home team. */
-   outcomeOne: string;
-   /** Draw. */
-   outcomeTwo: string;
-   /** Second team, typically the away team. */
-   outcomeThree: string;
-   /** Total pool for first team. */
-   poolOne: number;
-   /** Total pool for draw. */
-   poolTwo: number;
-   /** Total pool for second team. */
-   poolThree: number;
-   /** Overall total pool, sum of all other pools. */
-   totalPool: number;
-   /** Current state of the Betting Event. */
-   state: number;
-   /** Index of the outcome that won. */
-   winningIndex: number;
-   /** Score of the first team. */
-   teamOneScore: number;
-   /** Score of the second team. */
-   teamTwoScore: number;
-   /** Kickoff time of the match. */
-   kickOffTime: Date;
+  /** First team, typically the home team. */
+  outcomeOne: string;
+  /** Draw. */
+  outcomeTwo: string;
+  /** Second team, typically the away team. */
+  outcomeThree: string;
+  /** Total pool for first team. */
+  poolOne: number;
+  /** Total pool for draw. */
+  poolTwo: number;
+  /** Total pool for second team. */
+  poolThree: number;
+  /** Overall total pool, sum of all other pools. */
+  totalPool: number;
+  /** Current state of the Betting Event. */
+  state: number;
+  /** Index of the outcome that won. */
+  winningIndex: number;
+  /** Score of the first team. */
+  teamOneScore: number;
+  /** Score of the second team. */
+  teamTwoScore: number;
+  /** Kickoff time of the match. */
+  kickOffTime: Date;
 }
 
 export interface IUserBetInfo {
-   /** How many Wei the user placed in the bet. */
-   amount: number;
-   /** If the user has been paid yet or not. */
-   paid: boolean;
-   /** How much the user won on the bet. */
-   winnings: number;
-   /** Index of the outcome user bet on. */
-   outcomeIndex: number;
-   /** Address of the BetEvent */
-   address: string;
+  /** How many Wei the user placed in the bet. */
+  amount: number;
+  /** If the user has been paid yet or not. */
+  paid: boolean;
+  /** How much the user won on the bet. */
+  winnings: number;
+  /** Index of the outcome user bet on. */
+  outcomeIndex: number;
+  /** Address of the BetEvent */
+  address: string;
 }
 
 /**
@@ -56,8 +56,8 @@ export interface IUserBetInfo {
  * @returns amount of BetEvents managed by the smart contract.
  */
 const _getAmountOfBets = async (account: string): Promise<number> => {
-   const length = await managerContract.methods.length().call({ from: account });
-   return length;
+  const length = await managerContract.methods.length().call({ from: account });
+  return length;
 };
 
 /**
@@ -66,12 +66,12 @@ const _getAmountOfBets = async (account: string): Promise<number> => {
  * @returns Array of addresses of Betting contracts.
  */
 const _getAllBets = async (account: string): Promise<string[]> => {
-   const length = await _getAmountOfBets(account);
-   const addresses: string[] = [];
-   for (let i = 0; i < length; i++) {
-      addresses[i] = await managerContract.methods.betEvents(i).call({ from: account });
-   }
-   return addresses;
+  const length = await _getAmountOfBets(account);
+  const addresses: string[] = [];
+  for (let i = 0; i < length; i++) {
+    addresses[i] = await managerContract.methods.betEvents(i).call({ from: account });
+  }
+  return addresses;
 };
 
 /**
@@ -81,15 +81,15 @@ const _getAllBets = async (account: string): Promise<string[]> => {
  * @returns Array of addresses of Betting contracts that user placed bet on. 
  */
 const _getPlacedBets = async (account: string, betEvents: string[]): Promise<string[]> => {
-   const placedBets: string[] = [];
-   for (const betEvent of betEvents) {
-      bettingContract.options.address = betEvent;
-      const bet = await bettingContract.methods.bets(account).call({ from: account });
-      if (bet.amount !== '0') {
-         placedBets.push(betEvent);
-      }
-   }
-   return placedBets;
+  const placedBets: string[] = [];
+  for (const betEvent of betEvents) {
+    bettingContract.options.address = betEvent;
+    const bet = await bettingContract.methods.bets(account).call({ from: account });
+    if (bet.amount !== '0') {
+      placedBets.push(betEvent);
+    }
+  }
+  return placedBets;
 };
 
 /**
@@ -99,15 +99,15 @@ const _getPlacedBets = async (account: string, betEvents: string[]): Promise<str
  * @returns Array of bets for which the event is not over.
  */
 const _getAvailableBets = async (account: string, betEvents: string[]): Promise<string[]> => {
-   const availableBets: string[] = [];
-   for (const betEvent of betEvents) {
-      bettingContract.options.address = betEvent;
-      const state = await bettingContract.methods.state().call({ from: account });
-      if (state < 2) {
-         availableBets.push(betEvent);
-      }
-   }
-   return availableBets;
+  const availableBets: string[] = [];
+  for (const betEvent of betEvents) {
+    bettingContract.options.address = betEvent;
+    const state = await bettingContract.methods.state().call({ from: account });
+    if (state < 2) {
+      availableBets.push(betEvent);
+    }
+  }
+  return availableBets;
 };
 
 /**
@@ -117,37 +117,36 @@ const _getAvailableBets = async (account: string, betEvents: string[]): Promise<
  * @returns Information about a bet event.
  */
 const _getBetInfo = async (account: string, betEvent: string): Promise<IBetInfo> => {
-   bettingContract.options.address = betEvent;
-   const kickOff = await bettingContract.methods.kickOffTime().call({ from: account });
-   const kickOffTime = new Date(parseInt(kickOff, 10));
+  bettingContract.options.address = betEvent;
+  const kickOff = await bettingContract.methods.kickOffTime().call({ from: account });
+  const kickOffTime = new Date(parseInt(kickOff, 10));
+  const outcomeOne = await bettingContract.methods.outcomeOne().call({ from: account });
+  const outcomeTwo = await bettingContract.methods.outcomeTwo().call({ from: account });
+  const outcomeThree = await bettingContract.methods.outcomeThree().call({ from: account });
+  const poolOne = await bettingContract.methods.totalPools(0).call({ from: account });
+  const poolTwo = await bettingContract.methods.totalPools(1).call({ from: account });
+  const poolThree = await bettingContract.methods.totalPools(2).call({ from: account });
+  const totalPool = await bettingContract.methods.totalPools(3).call({ from: account });
+  const state = await bettingContract.methods.state().call({ from: account });
+  const winningIndex = await bettingContract.methods.winningIndex().call({ from: account });
+  const teamOneScore = await bettingContract.methods.teamOneScore().call({ from: account });
+  const teamTwoScore = await bettingContract.methods.teamTwoScore().call({ from: account });
 
-   const outcomeOne = await bettingContract.methods.outcomeOne().call({ from: account });
-   const outcomeTwo = await bettingContract.methods.outcomeTwo().call({ from: account });
-   const outcomeThree = await bettingContract.methods.outcomeThree().call({ from: account });
-   const poolOne = await bettingContract.methods.totalPools(0).call({ from: account });
-   const poolTwo = await bettingContract.methods.totalPools(1).call({ from: account });
-   const poolThree = await bettingContract.methods.totalPools(2).call({ from: account });
-   const totalPool = await bettingContract.methods.totalPools(3).call({ from: account });
-   const state = await bettingContract.methods.state().call({ from: account });
-   const winningIndex = await bettingContract.methods.winningIndex().call({ from: account });
-   const teamOneScore = await bettingContract.methods.teamOneScore().call({ from: account });
-   const teamTwoScore = await bettingContract.methods.teamTwoScore().call({ from: account });
-
-   const betInfo: IBetInfo = {
-      kickOffTime,
-      outcomeOne,
-      outcomeTwo,
-      outcomeThree,
-      poolOne,
-      poolTwo,
-      poolThree,
-      totalPool,
-      state,
-      winningIndex,
-      teamOneScore,
-      teamTwoScore,
-   };
-   return betInfo;
+  const betInfo: IBetInfo = {
+    kickOffTime,
+    outcomeOne,
+    outcomeTwo,
+    outcomeThree,
+    poolOne,
+    poolTwo,
+    poolThree,
+    totalPool,
+    state,
+    winningIndex,
+    teamOneScore,
+    teamTwoScore,
+  };
+  return betInfo;
 };
 
 /**
@@ -157,16 +156,16 @@ const _getBetInfo = async (account: string, betEvent: string): Promise<IBetInfo>
  * @returns Information about a bet a user placed.
  */
 const _getUserBetInfo = async (account: string, betEvent: string): Promise<IUserBetInfo> => {
-   bettingContract.options.address = betEvent;
-   const info = await bettingContract.methods.bets(account).call({ from: account });
-   const userBetInfo: IUserBetInfo = {
-      amount: info.amount,
-      paid: info.paid,
-      outcomeIndex: info.outcomeIndex,
-      winnings: info.winnings,
-      address: betEvent,
-   };
-   return userBetInfo;
+  bettingContract.options.address = betEvent;
+  const info = await bettingContract.methods.bets(account).call({ from: account });
+  const userBetInfo: IUserBetInfo = {
+    amount: info.amount,
+    paid: info.paid,
+    outcomeIndex: info.outcomeIndex,
+    winnings: info.winnings,
+    address: betEvent,
+  };
+  return userBetInfo;
 };
 
 /**
@@ -176,12 +175,12 @@ const _getUserBetInfo = async (account: string, betEvent: string): Promise<IUser
  * @param value Amount of Ether the user is betting.
  */
 export const placeBet = async (betEvent: string, outcomeIndex: number, value: string) => {
-   bettingContract.options.address = betEvent;
-   const accounts = await web3.eth.getAccounts();
-   bettingContract.methods.placeBet(outcomeIndex).send({
-      from: accounts[0],
-      value: web3.utils.toWei(value, 'ether'),
-   });
+  bettingContract.options.address = betEvent;
+  const accounts = await web3.eth.getAccounts();
+  bettingContract.methods.placeBet(outcomeIndex).send({
+    from: accounts[0],
+    value: web3.utils.toWei(value, 'ether'),
+  });
 };
 
 /**
@@ -190,9 +189,9 @@ export const placeBet = async (betEvent: string, outcomeIndex: number, value: st
  * @param outcomeIndex Index of the outcome that the user is changing their bet to.
  */
 export const changeBet = async (betEvent: string, outcomeIndex: number) => {
-   bettingContract.options.address = betEvent;
-   const accounts = await web3.eth.getAccounts();
-   bettingContract.methods.changeBet(outcomeIndex).send({ from: accounts[0] });
+  bettingContract.options.address = betEvent;
+  const accounts = await web3.eth.getAccounts();
+  bettingContract.methods.changeBet(outcomeIndex).send({ from: accounts[0] });
 };
 
 /**
@@ -200,11 +199,11 @@ export const changeBet = async (betEvent: string, outcomeIndex: number) => {
  * @param betEvent Address of the Betting contract.
  */
 export const claimWinnings = async (betEvent: string) => {
-   try {
-      bettingContract.options.address = betEvent;
-      const accounts = await web3.eth.getAccounts();
-      bettingContract.methods.claimWinnings().send({ from: accounts[0] });
-   } catch { }
+  try {
+    bettingContract.options.address = betEvent;
+    const accounts = await web3.eth.getAccounts();
+    bettingContract.methods.claimWinnings().send({ from: accounts[0] });
+  } catch { }
 };
 
 /**
@@ -212,13 +211,13 @@ export const claimWinnings = async (betEvent: string) => {
  * @returns Array of addresses of Betting contracts that user placed bet on. 
  */
 export const getPlacedBets = async (): Promise<string[]> => {
-   let placedBets: string[] = [];
-   try {
-      const accounts = await web3.eth.getAccounts();
-      const bets = await _getAllBets(accounts[0]);
-      placedBets = await _getPlacedBets(accounts[0], bets);
-   } catch { }
-   return placedBets;
+  let placedBets: string[] = [];
+  try {
+    const accounts = await web3.eth.getAccounts();
+    const bets = await _getAllBets(accounts[0]);
+    placedBets = await _getPlacedBets(accounts[0], bets);
+  } catch { }
+  return placedBets;
 };
 
 /**
@@ -226,13 +225,28 @@ export const getPlacedBets = async (): Promise<string[]> => {
  * @returns Array of bets for which the event is not over.
  */
 export const getAvailableBets = async (): Promise<string[]> => {
-   let availableBets: string[] = [];
-   try {
-      const accounts = await web3.eth.getAccounts();
-      const bets = await _getAllBets(accounts[0]);
-      availableBets = await _getAvailableBets(accounts[0], bets);
-   } catch { }
-   return availableBets;
+  let availableBets: string[] = [];
+  try {
+    const accounts = await web3.eth.getAccounts();
+    const bets = await _getAllBets(accounts[0]);
+    availableBets = await _getAvailableBets(accounts[0], bets);
+  } catch { }
+  return availableBets;
+};
+
+/**
+ * Returns an array of bets for which the event is not over from a
+ * list contract addresses.
+ * @param betEvents Array of addresses of Betting contracts.
+ * @returns Array of bets for which the event is not over.
+ */
+export const getAvailableBetsFromList = async (betEvents: string[]): Promise<string[]> => {
+  let availableBets: string[] = [];
+  try {
+    const accounts = await web3.eth.getAccounts();
+    availableBets = await _getAvailableBets(accounts[0], betEvents);
+  } catch { }
+  return availableBets;
 };
 
 /**
@@ -241,12 +255,12 @@ export const getAvailableBets = async (): Promise<string[]> => {
  * @returns Information about a match that a bet is a available for.
  */
 export const getBetInfo = async (betEvent: string): Promise<IBetInfo> => {
-   let betInfo: any = {};
-   try {
-      const accounts = await web3.eth.getAccounts();
-      betInfo = await _getBetInfo(accounts[0], betEvent);
-   } catch { }
-   return betInfo;
+  let betInfo: any = {};
+  try {
+    const accounts = await web3.eth.getAccounts();
+    betInfo = await _getBetInfo(accounts[0], betEvent);
+  } catch { }
+  return betInfo;
 };
 
 /**
@@ -255,10 +269,77 @@ export const getBetInfo = async (betEvent: string): Promise<IBetInfo> => {
  * @returns Information about a bet that a user has placed.
  */
 export const getUserBetInfo = async (betEvent: string): Promise<IUserBetInfo> => {
-   let userBetInfo: any = {};
-   try {
-      const accounts = await web3.eth.getAccounts();
-      userBetInfo = await _getUserBetInfo(accounts[0], betEvent);
-   } catch { }
-   return userBetInfo;
+  let userBetInfo: any = {};
+  try {
+    const accounts = await web3.eth.getAccounts();
+    userBetInfo = await _getUserBetInfo(accounts[0], betEvent);
+  } catch { }
+  return userBetInfo;
 };
+
+/**
+ * Used to check if a user is logged in.
+ * @returns A boolean signifying if a user is logged in or not.
+ */
+export const isUserLoggedIn = async (): Promise<boolean> => {
+  let userLoggedIn = false;
+  try {
+    const accounts = await web3.eth.getAccounts();
+    if (accounts[0].length > 0) {
+      userLoggedIn = true;
+    }
+  } catch{ }
+  return userLoggedIn;
+}
+
+/**
+ * Returns an array of club names that are in our system.
+ * @returns An array with club names that are in our system.
+ */
+export const getAllClubs = (): string[] => {
+  const clubs: string[] = [
+    "Arsenal",
+    "Bournemouth",
+    "Brighton",
+    "Burnley",
+    "Chelsea",
+    "Crystal Palace",
+    "Everton",
+    "Huddersfield",
+    "Leicester",
+    "Liverpool",
+    "Man City",
+    "Man Utd",
+    "Newcastle",
+    "Southampton",
+    "Stoke",
+    "Swansea",
+    "Spurs",
+    "Watford",
+    "West Brom",
+    "West Ham"]
+  return clubs;
+}
+
+/**
+ * Returns an array of bets for a specific club.
+ * @param club The name of the club.
+ * @returns An array of the bets for a specific club.
+ */
+export const getGamesByClub = async (club: string): Promise<string[]> => {
+  let gamesByClub: string[] = [];
+  try {
+    const accounts = await web3.eth.getAccounts();
+    const bets = await _getAllBets(accounts[0]);
+    for (const betEvent of bets) {
+      bettingContract.options.address = betEvent;
+      const teamOne = await bettingContract.methods.outcomeOne().call({ from: accounts[0] });
+      const teamTwo = await bettingContract.methods.outcomeOne().call({ from: accounts[0] });
+
+      if (teamOne === club || teamTwo === club) {
+        gamesByClub.push(betEvent);
+      }
+    }
+  } catch{ }
+  return gamesByClub;
+}
