05cc1c0cde848213716b94286f9970c5ac247fff
Swapnil Raj <raj.swapnil99@gmail.com>
2018-03-18 15:18:03 +0000

    Add partial implementation of contract store

--- src/stores/root.ts
+++ src/stores/root.ts
@@ -6,24 +6,28 @@ import { combineEpics, Epic } from 'redux-observable';
 
 import { combineReducers } from 'redux';
 
+import { contract, ContractActions, fetchAvailableEpic, IContractsState } from './contract';
 import { ISidebarState, sidebar, SidebarActions } from './sidebar';
-
 /**
  * Combined application actions interface
  */
-export type Actions = SidebarActions;
+export type Actions =
+  SidebarActions |
+  ContractActions;
 
 /**
  * Combined application state interface
  */
 export interface IState {
   sidebar: ISidebarState;
+  contract: IContractsState;
 }
 
-const epics: Array<Epic<Actions, IState, any>> = [].filter(epic => epic !== null) as Array<Epic<Actions, IState, any>>;
+const epics: Array<Epic<Actions, IState, any>> = [fetchAvailableEpic].filter(epic => epic !== null) as Array<Epic<Actions, IState, any>>;
 
 export const rootEpic = combineEpics<Actions, IState>(...epics);
 
 export const rootReducer = combineReducers<IState>({
   sidebar,
+  contract,
 });
