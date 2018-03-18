a11e5819e2ede66bcacf99358a955b7f068f5671
Swapnil Raj <raj.swapnil99@gmail.com>
2018-03-18 18:14:18 +0000

    Add placeBet action to store contract * Add placedBets array to state

--- src/stores/contract.ts
+++ src/stores/contract.ts
@@ -7,7 +7,7 @@ import { Epic } from 'redux-observable';
 import { Actions, IState } from './root';
 
 import { fromPromise } from 'rxjs/observable/fromPromise';
-import { getAvailableBets } from '../ethereum/contract-interaction';
+import { getAvailableBets, getPlacedBets } from '../ethereum/contract-interaction';
 
 /**
  * Action to issue a fetch request for all available bets
@@ -33,23 +33,48 @@ export const sucessAvailableBets = (availableBets: ISuccessAvailableBets['availa
   availableBets,
 });
 
+/**
+ * Action to placeBet
+ */
+interface IPlaceBet {
+  type: 'PLACE_BET';
+  betEvent: string;
+  outcomeIndex: string;
+  value: string;
+}
+const PLACE_BET: IPlaceBet['type'] = 'PLACE_BET';
+export const placeBet = (
+  betEvent: IPlaceBet['betEvent'],
+  outcomeIndex: IPlaceBet['outcomeIndex'],
+  value: IPlaceBet['value'],
+) => ({
+  type: PLACE_BET,
+  betEvent,
+  outcomeIndex,
+  value,
+});
+
 export const fetchAvailableEpic: Epic<Actions, IState> = action$ =>
   action$.ofType(FETCH_AVAILABLE_BETS).mergeMap(() => fromPromise(getAvailableBets()).map(sucessAvailableBets));
 
-export type ContractActions = IFetchAvailableBets | ISuccessAvailableBets;
+export type ContractActions = IFetchAvailableBets | ISuccessAvailableBets | IPlaceBet;
 
 export interface IContractsState {
   availableBets: string[];
+  placedBets: string[];
 }
 
 export const defaultContractsState: IContractsState = {
   availableBets: [],
+  placedBets: [],
 };
 
 export const contract = (state: IContractsState = defaultContractsState, action: ContractActions): IContractsState => {
   switch (action.type) {
     case SUCESS_AVAILABLE_BETS:
       return { ...state, availableBets: action.availableBets };
+    case PLACE_BET:
+      return { ...state, placedBets: state.placedBets.concat(action.betEvent) };
     default:
       return state;
   }
